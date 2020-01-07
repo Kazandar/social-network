6db077c85beffd47bd42e15727048ba71558e10c
Igor Dolganin <KazandarTV@gmail.com>
2020-01-07 15:48:55 +0300

    remove div wrapper

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './styles/App.scss';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import Friendbar from './components/Friendbar/Friendbar';
+import Profile from './components/Profile/Profile';
 
 const App = () => (
     <div className='app-wrapper'>
         <Header/>
         <Navbar/>
-        <div className='content'>
-            <Profile/>
-        </div>
+        <Profile/>
         <Friendbar/>
     </div>
 );
